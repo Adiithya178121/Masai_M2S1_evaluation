e5f8f7933ffc497cb4c10a2deb0ca33ba3260ad9
Adiithya178121 <adithya178121@gmail.com>
2025-03-23 12:18:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import create_ScoreManager from './scoreManager.js';
 const scoreManager = create_ScoreManager ();
-scoreManager.
+scoreManager.add_score("Alice", 85);
+scoreManager.add_score("Bob", 72);
+scoreManager.add_score("Charlie", 90);
+scoreManager.add_score("David", 58);
+
+console.log(scoreManager.get_scores());
+console.log(scoreManager.get_sortedScores());
+console.log(scoreManager.get_filteredScores(60));
+console.log(scoreManager.get_averagescore());
