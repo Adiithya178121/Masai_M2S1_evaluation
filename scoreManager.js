e3561b27b1b4c320730ae26d93b820f70852117f
Adiithya178121 <adithya178121@gmail.com>
2025-03-23 12:09:00 +0530

    Create scoreManager.js

---
+++ scoreManager.js
@@ -0,0 +1,24 @@
+const create_ScoreManager = () => {
+    const scores = [];
+    return {
+        adds_score(name , score) {
+            scores.push({name,score});
+        },
+        get_scores(){
+            return socres;
+        },
+        get_sortedscores() {
+            return [...scores].sort((a,b) => b.score - a.score);
+        },
+        get_filteredscores (threshold){
+            return scores.filter(student => student.score > threshold);
+},
+        get_avgeragescore() {
+            if(scores.length === 0) { 
+                return 0;
+                const total = scores.reduce ((sum , student) => sum + sutdent,0)
+                return total/scores.length
+        }
+ };
+};
+export default create_ScoreManager
